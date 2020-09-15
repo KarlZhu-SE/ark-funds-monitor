475114ac82b9aaa58e3f0af2f2ec2047d15173dc
IssueX <shawnzhuxiangchen@gmail.com>
2020-09-15 01:39:59 -0400

    Updates

---
+++ precache-manifest.02ab9ec28308b7db69a3a9cdc05ea01c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54a84526db487a534807270792cecdfe",
+    "url": "/ark-funds-monitor/index.html"
+  },
+  {
+    "revision": "2da84976020af653d289",
+    "url": "/ark-funds-monitor/static/css/2.70891016.chunk.css"
+  },
+  {
+    "revision": "79d44bedf72441ecc4cb",
+    "url": "/ark-funds-monitor/static/css/main.29ac6ec5.chunk.css"
+  },
+  {
+    "revision": "2da84976020af653d289",
+    "url": "/ark-funds-monitor/static/js/2.3501f491.chunk.js"
+  },
+  {
+    "revision": "7f1d4a68729d80bf531830879792688e",
+    "url": "/ark-funds-monitor/static/js/2.3501f491.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "79d44bedf72441ecc4cb",
+    "url": "/ark-funds-monitor/static/js/main.d39cfc79.chunk.js"
+  },
+  {
+    "revision": "80c37d62bc7c36e93c36",
+    "url": "/ark-funds-monitor/static/js/runtime-main.3ca9b8fc.js"
+  }
+]);
