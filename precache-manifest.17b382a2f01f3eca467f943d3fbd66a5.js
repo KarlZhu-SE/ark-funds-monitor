180af349862457691c57f33f50d90510fe83b055
KarlZhu-SE <shawnzhuxiangchen@gmail.com>
2021-01-03 02:35:08 +0000

    Deploying to gh-pages from  @ 7308929a369c3f3dc1630b065c7b9e0cde1199c0 🚀

--- precache-manifest.17b382a2f01f3eca467f943d3fbd66a5.js
+++ precache-manifest.17b382a2f01f3eca467f943d3fbd66a5.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "a2b354f3a505fecdea2fbe474cc0bfeb",
+    "revision": "6956242d15cc8967103da272834b759e",
     "url": "/ark-funds-monitor/index.html"
   },
   {
@@ -8,8 +8,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/ark-funds-monitor/static/css/2.70891016.chunk.css"
   },
   {
-    "revision": "bbaa2cbd63ccc020b57d",
-    "url": "/ark-funds-monitor/static/css/main.b6781218.chunk.css"
+    "revision": "db1da9ee775cdcefcf12",
+    "url": "/ark-funds-monitor/static/css/main.5bdd5857.chunk.css"
   },
   {
     "revision": "72f71d4b86ea48516d64",
@@ -20,8 +20,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/ark-funds-monitor/static/js/2.5ed9122e.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "bbaa2cbd63ccc020b57d",
-    "url": "/ark-funds-monitor/static/js/main.b6aa6b43.chunk.js"
+    "revision": "db1da9ee775cdcefcf12",
+    "url": "/ark-funds-monitor/static/js/main.ada54e97.chunk.js"
   },
   {
     "revision": "80c37d62bc7c36e93c36",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/ark-funds-monitor/precache-manifest.0bfb9fd5caf2123f6775276a9b0c7abe.js"
+  "/ark-funds-monitor/precache-manifest.17b382a2f01f3eca467f943d3fbd66a5.js"
 );
 
 self.addEventListener('message', (event) => {
