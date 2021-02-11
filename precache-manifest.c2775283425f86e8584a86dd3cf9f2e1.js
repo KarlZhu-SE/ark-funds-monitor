3df89fd3644d7ad0e4f4636239802442ce932920
KarlZhu-SE <shawnzhuxiangchen@gmail.com>
2021-02-11 01:45:34 +0000

    Deploying to gh-pages from  @ 6f7cdd39012db77edf8b5d608eecebc29bc4cea2 🚀

--- precache-manifest.c2775283425f86e8584a86dd3cf9f2e1.js
+++ precache-manifest.c2775283425f86e8584a86dd3cf9f2e1.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "8104f35b5b5fad0c7cf2e20acb1af376",
+    "revision": "9cbbe791b72cc04a45d4cc803854e154",
     "url": "/ark-funds-monitor/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/ark-funds-monitor/static/css/2.70891016.chunk.css"
   },
   {
-    "revision": "ff3e2e385f64bfa27dd3",
+    "revision": "f7e3da850c02fa140c1d",
     "url": "/ark-funds-monitor/static/css/main.136f4053.chunk.css"
   },
   {
@@ -20,8 +20,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/ark-funds-monitor/static/js/2.5ed9122e.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "ff3e2e385f64bfa27dd3",
-    "url": "/ark-funds-monitor/static/js/main.96ddcabe.chunk.js"
+    "revision": "f7e3da850c02fa140c1d",
+    "url": "/ark-funds-monitor/static/js/main.87eec4fb.chunk.js"
   },
   {
     "revision": "80c37d62bc7c36e93c36",
